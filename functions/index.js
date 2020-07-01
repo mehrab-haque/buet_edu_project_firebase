ec578330509bb5a80e744e38b11c13d896520e71
mehrab-haque <mehrab.haque.0001@gmail.com>
2020-07-01 08:00:27 +0600

    added timestamp

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,10 @@ const Firestore = require('@google-cloud/firestore');
 admin.initializeApp();
 
 exports.addProblem = functions.https.onRequest((request, response) => {
+  var data=JSON.parse(request.body);
+  data["timestamp"]=Date.now();
   var ref_problem=admin.firestore().collection("problem").doc();
-  return ref_problem.set(JSON.parse(request.body)).then(()=>{
+  return ref_problem.set(data).then(()=>{
     return response.send({
       "status":"done"
     });
